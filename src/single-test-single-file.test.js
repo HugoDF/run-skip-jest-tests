dbba11a226b401bb80ff28b97c76bdbc53bee902
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-10-02 19:02:50 +0100

    format

--- src/single-test-single-file.test.js
+++ src/single-test-single-file.test.js
@@ -2,7 +2,7 @@ describe('my suite', () => {
   test.only('my only true test', () => {
     expect(1 + 1).toEqual(2);
   });
-  // should fail, but isn't even run
+  // Should fail, but isn't even run
   test('my only true test', () => {
     expect(1 + 1).toEqual(1);
   });
